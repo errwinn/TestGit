ffea3ed2c0a81629043db3679e8e2b1f1d03c6b4
errwinn <erwinerw28@gmail.com>
2024-05-06 11:27:24 +0700

    Initial Commit

---
+++ TestGit/TestGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitApp.swift
+//  TestGit
+//
+//  Created by Erwin on 06/05/24.
+//
+
+import SwiftUI
+
+@main
+struct TestGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
